ca4e808b7dc098b3f2e0db9af211e09a75066563
Patrik Nilsson <patriknil90@gmail.com>
2019-08-10 11:55:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <br />
+      <br />
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
+}
+
+export default App;
